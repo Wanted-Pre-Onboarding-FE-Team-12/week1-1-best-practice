1f19885ea35e7bb18d9f38962a5593bfa5d067e9
정현준 <slosh@jeonghyeonjun-ui-MacBookAir.local>
2022-08-31 21:09:24 +0900

    [#9] style : eslint 적용

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,67 +1,64 @@
-import React, { useContext, useState } from "react";
-import ModalContext from "store/modalStore";
-import TodoContext from "store/todoStore";
+import React, { useContext, useState } from 'react';
+import ModalContext from 'store/modalStore';
+import TodoContext from 'store/todoStore';
 
 // CSS
-import { Modal } from "@mui/material";
-import { Box } from "./style";
+import { Modal } from '@mui/material';
+import { Box } from './style';
 
 const CustomModal = ({ content }) => {
-    const { modalOpen, setModalOpen, updateMode, setUpdateMode } =
-        useContext(ModalContext);
-    const { updateTodo, deleteTodo, todoId } = useContext(TodoContext);
-    const [newTodo, setNewTodo] = useState(content.todo);
+  const { modalOpen, setModalOpen, updateMode, setUpdateMode } = useContext(ModalContext);
+  const { updateTodo, deleteTodo, todoId } = useContext(TodoContext);
+  const [newTodo, setNewTodo] = useState(content.todo);
 
-    const onChangeHandler = (e) => {
-        setNewTodo(e.target.value);
-    };
+  const onChangeHandler = e => {
+    setNewTodo(e.target.value);
+  };
 
-    const onClose = () => {
-        setModalOpen(false);
-        setUpdateMode(false);
-    };
+  const onClose = () => {
+    setModalOpen(false);
+    setUpdateMode(false);
+  };
 
-    const onDelete = () => {
-        deleteTodo(todoId);
-        onClose();
-    };
+  const onDelete = () => {
+    deleteTodo(todoId);
+    onClose();
+  };
 
-    const onModify = () => {
-        updateTodo(content.id, {
-            todo: newTodo,
-            isCompleted: content.isCompleted,
-        });
-        onClose();
-    };
+  const onModify = () => {
+    updateTodo(content.id, {
+      todo: newTodo,
+      isCompleted: content.isCompleted,
+    });
+    onClose();
+  };
 
-    return (
-        <Modal
-            open={modalOpen}
-            onClose={onClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-            <Box>
-                <div className="modal-container">
-                    {updateMode ? (
-                        <div className="modify-container">
-                            <p>Todo 수정</p>
-                            <input value={newTodo} onChange={onChangeHandler} />
-                        </div>
-                    ) : (
-                        <p>삭제하시겠습니까?</p>
-                    )}
+  return (
+    <Modal
+      open={modalOpen}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box>
+        <div className="modal-container">
+          {updateMode ? (
+            <div className="modify-container">
+              <p>Todo 수정</p>
+              <input value={newTodo} onChange={onChangeHandler} />
+            </div>
+          ) : (
+            <p>삭제하시겠습니까?</p>
+          )}
 
-                    <div className="btn-container">
-                        <button onClick={onClose}>취소</button>
-                        <button onClick={updateMode ? onModify : onDelete}>
-                            확인
-                        </button>
-                    </div>
-                </div>
-            </Box>
-        </Modal>
-    );
+          <div className="btn-container">
+            <button onClick={onClose}>취소</button>
+            <button onClick={updateMode ? onModify : onDelete}>확인</button>
+          </div>
+        </div>
+      </Box>
+    </Modal>
+  );
 };
 
 export default CustomModal;
--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,36 +1,36 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-const useInput = (validateValue) => {
-    const [enteredValue, setEnteredValue] = useState("");
-    const [IsTouched, setIsTouched] = useState(false);
+const useInput = validateValue => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [IsTouched, setIsTouched] = useState(false);
 
-    // Validation
-    const valueIsValid = validateValue(enteredValue);
-    const hasError = !valueIsValid && IsTouched;
+  // Validation
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && IsTouched;
 
-    // onChange
-    const valueChangeHandler = (e) => {
-        setEnteredValue(e.target.value);
-    };
+  // onChange
+  const valueChangeHandler = e => {
+    setEnteredValue(e.target.value);
+  };
 
-    // onBlur
-    const inputBlurHandler = () => {
-        setIsTouched(true);
-    };
+  // onBlur
+  const inputBlurHandler = () => {
+    setIsTouched(true);
+  };
 
-    // onRest
-    const resetHandler = () => {
-        setEnteredValue("");
-    };
+  // onRest
+  const resetHandler = () => {
+    setEnteredValue('');
+  };
 
-    return {
-        value: enteredValue,
-        isValid: valueIsValid,
-        hasError,
-        valueChangeHandler,
-        inputBlurHandler,
-        resetHandler,
-    };
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    resetHandler,
+  };
 };
 
 export default useInput;
